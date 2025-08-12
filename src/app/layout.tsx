a3c165ec148fa25d0a5bd35cb0e97d3fd977024f
Jack212-dot <jf015064@gmail.com>
2025-08-12 22:18:44 +0500

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,8 +7,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-black"
-      >
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CRXJVEHW2V"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CRXJVEHW2V');
+            `,
+          }}
+        />
+      </head>
+      <body className="bg-black">
         {children}
       </body>
     </html>
